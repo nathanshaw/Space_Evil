a065da411b60db8bc2dd2141c8d7dd67b52382eb
nathanshaw <nathanshawsemail@gmail.com>
2017-01-08 14:00:27 -0800

    fixed issue with enemy ships playing explosion sound when they fire their first bullet

--- Assets/Scripts/Movement/ZigZagMover01.cs
+++ Assets/Scripts/Movement/ZigZagMover01.cs
@@ -31,7 +31,7 @@ public class ZigZagMover01 : MonoBehaviour {
 		// flip the velocity every betweenTimes seconds
 		while (true) {
 			rigidbody.velocity = Vector3.Scale (rigidbody.velocity, 
-				new Vector3 (0.0f, 0.0f, -1.0f)); 
+				new Vector3 (1.0f, 1.0f, -1.0f)); 
 			yield return new WaitForSeconds (betweenTime);
 		}
 	}
