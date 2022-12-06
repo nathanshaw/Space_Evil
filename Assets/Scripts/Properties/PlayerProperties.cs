7e9d5454a4fa23bb8cda73fe89da28f13142d8c7
Nathan Villicaña-Shaw <nathanshawsemail@gmail.com>
2022-12-06 10:25:20 -0800

    Working on updating project to more recent version of Unity

---
+++ Assets/Scripts/Properties/PlayerProperties.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponProperties : MonoBehaviour {
+	 
+
+	public float xSpeed;
+	public float ySpeed;
+
+	public GameObject Explosion;
+
+	public int sourcePlayerID;
+
+	public float minDamage;
+	public float maxDamage;
+	public float damage;
+
+	public float UpdateDamage ( float damDelta ) {
+		damage += damDelta;
+		if (damage < minDamage) {
+			damage = minDamage;
+		} else if (damage > maxDamage) {
+			damage = maxDamage;
+		}
+		return damage;
+	}
+
+}
